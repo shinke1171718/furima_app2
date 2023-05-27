5f833c48ae69f4267068e8f01ec7408e7eb8d41a
Apple <apple@ShinkenoMacBook-3.local>
2023-05-27 20:16:07 +0900

    “ログアウトの機能を実装しました。”

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root to: 'items#index'
   get 'items/:id/cheackout', to: 'items#cheackout', as: 'item_cheackout'
   devise_for :users, controllers: { registrations: 'registrations' }
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   resources :items do
     member do
       get :cheackout
